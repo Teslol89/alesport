afc620a43e9bd4935d9bf66ebee04dd9bb5c151c
Marcos <marcos.info1989@gmail.com>
2026-03-26 15:26:02 +0100

    feat: Implementación del autenticador de google

--- mobile/alesport-app/src/api/auth.ts
+++ mobile/alesport-app/src/api/auth.ts
@@ -1,5 +1,19 @@
 import { baseApiUrl } from './config';
 
+export async function loginWithGoogle(idToken: string) {
+    const response = await fetch(`${baseApiUrl}/auth/google-login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id_token: idToken })
+    });
+    if (!response.ok) {
+        throw new Error('No se pudo iniciar sesión con Google.');
+    }
+    return response.json();
+}
+
 export async function loginUser(email: string, password: string) {
     const response = await fetch(`${baseApiUrl}/auth/login`, {
         method: 'POST',

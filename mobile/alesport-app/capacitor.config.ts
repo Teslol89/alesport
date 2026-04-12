8ec392a5bb6018702d0a59626f55c58904abf574
Marcos <marcos.info1989@gmail.com>
2026-04-12 12:06:57 +0200

    codemagic

--- mobile/alesport-app/capacitor.config.ts
+++ mobile/alesport-app/capacitor.config.ts
@@ -1,9 +1,16 @@
+/// <reference types="@capacitor/keyboard" />
 import type { CapacitorConfig } from '@capacitor/cli';
+import { KeyboardResize } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
   appId: 'com.alesport.app',
   appName: 'Alesport',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    Keyboard: {
+      resize: KeyboardResize.Ionic,
+    },
+  },
 };
 
 export default config;
